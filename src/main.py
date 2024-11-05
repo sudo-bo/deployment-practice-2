8bcc9d8b560a812d0ad150ff1d42c7a475506eaf
bo <bodrye.kam@gmail.com>
2024-11-05 12:24:58 -0500

    testing auto deployment

--- src/main.py
+++ src/main.py
@@ -29,5 +29,5 @@ app.include_router(smokeTest.router, prefix="/smoke-test")
 @app.get('/')
 def health():
     return {
-        "message": "OK 🚀"
+        "message": "OK - 🚀🚀🚀 TO THE MOON !"
     }
